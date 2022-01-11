f5111231012f1361601aaeafdbc3a3934083ab21
kyy119 <duddbs1994@naver.com>
2022-01-11 16:37:32 +0900

    태그 페이지 만들기

--- blog/urls.py
+++ blog/urls.py
@@ -10,6 +10,7 @@ from . import views
 
 #CBV(Class Based View)일때
 urlpatterns = [
+    path('tag/<str:slug>/', views.tag_page),
     path('category/<str:slug>/', views.category_page),
     path('<int:pk>/', views.PostDetail.as_view()),
     path('', views.PostList.as_view()),
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
-from .models import Post, Category
+from .models import Post, Category, Tag
 
 #CBV(Class Based View)로 블로스 포스트 목록 페이지 만들기
 class PostList(ListView):
@@ -43,6 +43,20 @@ def category_page(request, slug):
         }
     )
 
+
+def tag_page(request, slug):
+    tag = Tag.objects.get(slug=slug)
+    post_list = tag.post_set.all()
+    return render(
+        request,
+        'blog/post_list.html',
+        {
+            'post_list': post_list,
+            'categories': Category.objects.all(),
+            'no_category_post_count': Post.objects.filter(category=None).count(),
+            'tag': tag
+        }
+    )
 
 
 #FBV(Function Based view) 블리스 리스트 페이지
