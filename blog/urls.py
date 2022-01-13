0b9b3d089c6142515fe1910fca7daa9f8cdc1971
kyy119 <duddbs1994@naver.com>
2022-01-13 21:44:46 +0900

    검색 기능 구현하기

--- blog/urls.py
+++ blog/urls.py
@@ -10,6 +10,7 @@ from . import views
 
 #CBV(Class Based View)일때
 urlpatterns = [
+    path('search/<str:q>/', views.PostSearch.as_view()),
     path('delete_comment/<int:pk>/', views.delete_comment),
     path('update_comment/<int:pk>/', views.CommentUpdate.as_view()),
     path('update_post/<int:pk>/', views.PostUpdate.as_view()),
--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,5 @@
 from django.core.exceptions import PermissionDenied
+from django.db.models import Q
 from django.shortcuts import render, redirect, get_object_or_404
 from django.utils.text import slugify
 from django.views.generic import ListView, DetailView, CreateView, UpdateView
@@ -178,6 +179,22 @@ def delete_comment(request, pk):
     else:
         raise PermissionDenied
 
+
+class PostSearch(PostList):
+    paginate_by = None
+
+    def get_queryset(self):
+        q = self.kwargs['q']
+        post_list = Post.objects.filter(
+            Q(title__contains=q) | Q(tags__name__contains=q)
+        ).distinct()
+        return post_list
+
+    def get_context_data(self, **kwargs):
+        context = super(PostSearch, self).get_context_data()
+        q = self.kwargs['q']
+        context['search_info'] = f'Search: {q} ({self.get_queryset().count()})'
+        return context
 
 #FBV(Function Based view) 블리스 리스트 페이지
 #def index(request):
