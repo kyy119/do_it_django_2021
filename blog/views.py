f09965f9e975e7b1842b00a1df609ad09187d81c
kyy119 <duddbs1994@naver.com>
2022-01-13 18:15:21 +0900

    댓글 삭제 기능 추가하기

--- blog/views.py
+++ blog/views.py
@@ -168,6 +168,17 @@ class CommentUpdate(LoginRequiredMixin, UpdateView):
             raise PermissionDenied
 
 
+def delete_comment(request, pk):
+    comment = get_object_or_404(Comment, pk=pk)
+    post = comment.post
+
+    if request.user.is_authenticated and request.user == comment.author:
+        comment.delete()
+        return redirect(post.get_absolute_url())
+    else:
+        raise PermissionDenied
+
+
 #FBV(Function Based view) 블리스 리스트 페이지
 #def index(request):
 #    posts = Post.objects.all().order_by('-pk')
